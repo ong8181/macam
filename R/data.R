33f06d8382ce4e3374dd9656a8b72911516721b2
Masayuki Ushio <ong8181@gmail.com>
2022-05-22 22:21:51 +0800

    vignettes edited

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' Demo sample data used for phyloseq object
+#'
+#' @format A data frame with 36 rows and 14 variables:
+#' @source \url{https://doi.org/10.1111/2041-210X.13204}
+"sample_sheet"
+
+
+#' Demo ASV table data used for phyloseq object
+#'
+#' @format A data frame with 36 rows and 358 variables:
+#' @source \url{https://doi.org/10.1111/2041-210X.13204}
+"asv_sheet"
+
+
+#' Demo taxa table data used for phyloseq object
+#'
+#' @format A data frame with 358 rows and 13 variables:
+#' @source \url{https://doi.org/10.1111/2041-210X.13204}
+"tax_sheet"
+
+
+#' Two species model time series
+#'
+#' @format A data frame with 1000 rows and 3 variables:
+#' \describe{
+#'   \item{time}{Time ID}
+#'   \item{x}{Species x}
+#'   \item{y}{Species y}
+#' }
+"data_2sp"
