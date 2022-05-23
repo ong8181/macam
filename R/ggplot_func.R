3b2086febe1f6ce5b222b378d2dcb19654877777
Masayuki Ushio <ong8181@gmail.com>
2022-05-23 10:02:06 +0800

    fug fixed

--- R/ggplot_func.R
+++ R/ggplot_func.R
@@ -1,6 +1,6 @@
 #' @title Power label function for ggplot2
 #' @description \code{label_10_to_power} transforms a numeric variable to a scientific notation.
-#' @param x Numeric. A numeric variable that will be converted to a sceitific notation.
+#' @param x Numeric. A numeric variable that will be converted to a scientific notation.
 #' @return Scientific notation.
 #' @export
 #' @examples
