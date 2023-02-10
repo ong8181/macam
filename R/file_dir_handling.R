61fc2102c3de1c5d8997d8d332d5a38b2ec0e1ec
Masayuki Ushio <ong8181@gmail.com>
2023-02-10 10:25:54 +0800

    updated to v0.1.0

--- R/file_dir_handling.R
+++ R/file_dir_handling.R
@@ -22,16 +22,19 @@ outdir_create <- function(save_dir_name = TRUE, str_end = 3, suffix = "Out") {
 #' @title Save workspace using an output directory name
 #' @description \code{save_workspace} saves current workspace in a user-specified output directory.
 #' @param workspace_dir Character. Workspace will be saved in this directory.
+#' @param workspace_file_name Character (optional). Workspace file name.
 #' @export
 #' @examples
 #' # output_dir <- outdir_create()
 #' # save_workspace(output_dir)
-save_workspace <- function(workspace_dir) {
+save_workspace <- function(workspace_dir, workspace_file_name = NULL) {
   # Create output_dir if it does not exists
   if (!dir.exists(workspace_dir)) dir.create(workspace_dir)
+  # Specify workspace file name
+  if (is.null(workspace_file_name)) workspace_file_name <- workspace_dir
   # Save workspace
   save(list = ls(all.names = TRUE),
-     file = paste0(workspace_dir, "/", workspace_dir, ".RData"))
+     file = paste0(workspace_dir, "/", workspace_file_name, ".RData"))
 }
 
 
@@ -40,14 +43,16 @@ save_workspace <- function(workspace_dir) {
 #' @param session_info_dir Character. Name of the session information directory.
 #' @param create_session_info_dir Logical. If `TRUE`, the function automatically creates an output directory for the session information
 #' @param str_end Integer. Trim `str_end` characters from the file name.
+#' @param suffix Character. Suffix that will be appended to the file name.
 #' @export
 #' @examples
 #' # output_dir <- outdir_create()
 #' # save_session_info(output_dir)
-save_session_info <- function(session_info_dir = "00_SessionInfo", create_session_info_dir = FALSE, str_end = 3) {
+save_session_info <- function(session_info_dir = "00_SessionInfo", create_session_info_dir = FALSE, str_end = 3, suffix = NULL) {
   # Extract the running srcript file name
   current_file_name <- rstudioapi::getSourceEditorContext()$path %>%
     basename %>% stringr::str_sub(end = - str_end)
+  if(!is.null(suffix)) current_file_name <- paste0(current_file_name, suffix)
   # Create session_info_dir if it does not exists
   if (!dir.exists(session_info_dir) & create_session_info_dir) {
     dir.create(session_info_dir)
